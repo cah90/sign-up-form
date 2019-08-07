4cdaae91a7430155217187f8e4a0d993d40d610f
cassia <cascrisbern@gmail.com>
2019-08-07 16:36:26 +0100

    changes on javascript

--- javascript/validation.js
+++ javascript/validation.js
@@ -8,33 +8,32 @@ $( document ).ready( function (){
     var passwordLogin = $(password).val();
 
     var pattern = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    
-    $("form").each(function(){
 
-        if (firstName.length === 0) {
-            $("#first_name").css("border", "2px solid hsl(0, 100%, 74%)");
-            $("#error_first_name").html($("#first_name").attr("placeholder") + " cannot be empty");
-        }    
-    
-        if (lastName.length === 0) {
-            $("#last_name").css("border", "2px solid hsl(0, 100%, 74%)");
-            $("#error_last_name").html($("#last_name").attr("placeholder") + " cannot be empty");
-        }  
-    
-        if (emailAddress.length === 0) {
-            $("#email").css("border", "2px solid hsl(0, 100%, 74%)");
-            $("#error_email").html($("#email").attr("placeholder") + " cannot be empty");
-        } else if (emailAddress !== pattern ) {
-            $("#email").css("border", "2px solid hsl(0, 100%, 74%)");
-            $("#error_email").html("Looks like this is not an email");
-        }
-    
-        if (passwordLogin.length === 0) {
-            $("#password").css("border", "2px solid hsl(0, 100%, 74%)");
-            $("#error_password").html($("#password").attr("placeholder") + " cannot be empty");
-        }  
-    });
 
+    if (firstName.length === 0) {
+        // $("#first_name").addClass("error");
+        $("#first_name").css("border", "2px solid hsl(0, 100%, 74%)");
+        $("#error_first_name").html($("#first_name").attr("placeholder") + " cannot be empty");
+    }    
+
+    if (lastName.length === 0) {
+        $("#last_name").css("border", "2px solid hsl(0, 100%, 74%)");
+        $("#error_last_name").html($("#last_name").attr("placeholder") + " cannot be empty");
+    }  
+
+    if (emailAddress.length === 0) {
+        $("#email").css("border", "2px solid hsl(0, 100%, 74%)");
+        $("#error_email").html($("#email").attr("placeholder") + " cannot be empty");
+    } else if (emailAddress !== pattern ) {
+        $("#email").css("border", "2px solid hsl(0, 100%, 74%)");
+        $("#error_email").html("Looks like this is not an email");
+    }
+
+    if (passwordLogin.length === 0) {
+        $("#password").css("border", "2px solid hsl(0, 100%, 74%)");
+        $("#error_password").html($("#password").attr("placeholder") + " cannot be empty");
+    }  
     
+
     });
 });
