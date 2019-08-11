36bb1ef0cc54b96febab9ce5fefdf12bbb43c0cc
cassia <cascrisbern@gmail.com>
2019-08-11 14:06:39 +0100

    improved validation

--- javascript/validation.js
+++ javascript/validation.js
@@ -2,38 +2,72 @@ $( document ).ready( function (){
 
     $( "button" ).on("click", function(){
 
-    var firstName = $(first_name).val();
-    var lastName = $(last_name).val();
-    var emailAddress = $(email).val();
-    var passwordLogin = $(password).val();
+    var firstName = $("#first_name").val();
+    var lastName = $("#last_name").val();
+    var emailAddress = $("#email").val();
+    var passwordLogin = $("#password").val();
+    var placeholder;
 
-    var pattern = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 
-    if (firstName.length === 0) {
-        // $("#first_name").addClass("error");
-        $("#first_name").css("border", "2px solid hsl(0, 100%, 74%)");
-        $("#error_first_name").html($("#first_name").attr("placeholder") + " cannot be empty");
-    }    
 
-    if (lastName.length === 0) {
-        $("#last_name").css("border", "2px solid hsl(0, 100%, 74%)");
-        $("#error_last_name").html($("#last_name").attr("placeholder") + " cannot be empty");
-    }  
-
-    if (emailAddress.length === 0) {
-        $("#email").css("border", "2px solid hsl(0, 100%, 74%)");
-        $("#error_email").html($("#email").attr("placeholder") + " cannot be empty");
-    } else if (emailAddress !== pattern ) {
-        $("#email").css("border", "2px solid hsl(0, 100%, 74%)");
-        $("#error_email").html("Looks like this is not an email");
+    if (firstName === "") {
+        placeholder = $("#first_name").attr("placeholder");
+        $("#first_name").addClass("error");
+        $("#first_name").attr("placeholder", "");
+        $("#error_first_name").html("First Name cannot be empty");
+    } else {
+        $("#first_name").removeClass("error");
+        $("#error_first_name").html("");
     }
 
-    if (passwordLogin.length === 0) {
-        $("#password").css("border", "2px solid hsl(0, 100%, 74%)");
-        $("#error_password").html($("#password").attr("placeholder") + " cannot be empty");
-    }  
-    
+    if (lastName === "") {
+        placeholder = $("#last_name").attr("placeholder");
+        $("#last_name").addClass("error");
+        $("#last_name").attr("placeholder", "");
+        $("#error_last_name").html("Last Name cannot be empty");
+    } else {
+        $("#last_name").removeClass("error");
+        $("#error_last_name").html("");
+    }
 
+    if (passwordLogin === "") {
+        placeholder = $("#password").attr("placeholder");
+        $("#password").addClass("error");
+        $("#password").attr("placeholder", "");
+        $("#error_password").html("Password cannot be empty");
+    } else {
+        $("#password").removeClass("error");
+        $("#error_password").html("");
+    }
+
+    emailValidation(emailAddress);
+
+    
     });
+
+    function emailValidation(email){
+
+        var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+        if (email === "") {
+            $("#email").addClass("error");
+            $("#email").attr("placeholder", "");
+            $("#error_email").html("Email cannot be empty");
+    
+        } else if (!email.match(pattern) ) {
+            $("#email").addClass("error");
+            $("#error_email").html("Looks like this is not an email");
+            $("#email").val("email@example.com");
+    
+        } else {
+            $("#email").removeClass("error");
+            $("#error_email").html("");
+
+    
+        }
+    }
+
+    
 });
